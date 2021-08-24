6ae05b44f0b1f3c7223db90f21b403a03cb6d9c1
Tushar Amrit <48964465+tushar-amrit-6@users.noreply.github.com>
2021-08-24 19:03:05 +0530

    Update LFU Cache O(logn).cpp

--- LFU Cache O(logn).cpp
+++ LFU Cache O(logn).cpp
@@ -1,82 +1,44 @@
+typedef pair<int,int> ii;
+
 class LFUCache {
-private:
-    int num;
-    int time;
-    int capacity;
-    struct Node{
-        int key;
-        int cnt;
-        int time;
-        Node(int key, int cnt, int time){
-            this->key = key;
-            this->cnt = cnt;
-            this->time = time;
-        }
-        bool operator < (const Node& a) const {
-            if (this->cnt == a.cnt) {
-                return this->time > a.time;
-            }
-            return this->cnt > a.cnt;
-        }
-    };
-    priority_queue<Node, vector<Node>> frequency;
-    unordered_map<int, int> cache;
-    unordered_map<int, int> count;
-    unordered_map<int, int> tag;
 public:
+    priority_queue<pair<int,ii>, vector<pair<int,ii>>, greater<pair<int,ii>>> pq;
+    unordered_map<int,int> m;
+    unordered_map<int,int> freq;
+    int cap;
+    int t=0;
     LFUCache(int capacity) {
-        this->time = 0;
-        this->num = 0;
-        this->capacity = capacity;
+        cap = capacity;
     }
     
     int get(int key) {
-        if (cache.find(key) == cache.end()) {
-            return -1;
-        }
-        count[key]++;
-        tag[key] = time;
-        time++;
-        return cache[key];
+        if(m.find(key)==m.end()) return -1;
+        put(key,m[key]);
+        return m[key];
+        
     }
     
     void put(int key, int value) {
-        if (cache.find(key) != cache.end()) {
-            cache[key] = value;
-            count[key]++;
-            tag[key] = time;
-        } else if (num < capacity) {
-            frequency.push(Node(key, 1, time));
-            cache[key] = value;
-            count[key] = 1;
-            tag[key] = time;
-            num++;
-        } else {
-            bool flag = false;
-            while(!frequency.empty()){
-                Node top = frequency.top();
-                frequency.pop();
-                if (top.cnt==count[top.key] && tag[top.key]==top.time) {
-                    ejectKey(top.key);
-                    flag = true;
-                    break;
-                }
-                frequency.push(Node(top.key, count[top.key], tag[top.key]));
-            }
-            if (flag) {
-                cache[key] = value;
-                count[key] = 1;
-                tag[key] = time;
-                frequency.push(Node(key, 1, time));
-            }
+        if(cap==0){
+            return;
         }
-        time++;
-        return ;
-    }
-private:
-    void ejectKey(int key){
-        count.erase(key);
-        cache.erase(key);
-        tag.erase(key);
+        if(m.find(key)==m.end()&&m.size()==cap){
+			/// capacity full
+            while(m.size()==cap){
+			// try to actually remove an element from our maps
+                auto x = pq.top();
+                if(freq[x.second.second]==x.first){
+				// this entry actually is a valid entry( freq[key] = freq_while_this_element_was_inserted)
+                    m.erase(x.second.second);
+                    freq.erase(x.second.second);
+                }
+                pq.pop();
+            }            
+        }
+		// fill priority_queue and increase time
+        m[key] = value;
+        freq[key]++;
+        pq.push({freq[key],{t++,key}});
+        
     }
 };
