131aa73db117406b827a88547d373207e84f09ac
Tushar Amrit <48964465+tushar-amrit-6@users.noreply.github.com>
2021-09-01 14:36:58 +0530

    Update Morris Travelsal.cpp

--- Morris Travelsal.cpp
+++ Morris Travelsal.cpp
@@ -29,3 +29,36 @@ public:
         return inorder; 
     }
 };
+
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preorder; 
+        
+        TreeNode* cur = root; 
+        while(cur != NULL) {
+            if(cur->left == NULL) {
+                preorder.push_back(cur->val); 
+                cur = cur->right; 
+            }
+            else {
+                TreeNode* prev = cur->left; 
+                while(prev->right != NULL && prev->right != cur) {
+                    prev = prev->right; 
+                }
+                
+                if(prev->right == NULL) {
+                    prev->right = cur;
+                    preorder.push_back(cur->val); 
+                    cur = cur->left; 
+                }
+                else {
+                    prev->right = NULL;
+                    cur = cur->right; 
+                }
+            }
+        }
+        return preorder; 
+    }
+};
