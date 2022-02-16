cd87250ab25455798e8f6ad45ffdc9e5879f960d
Tushar Amrit <48964465+tushar-amrit-6@users.noreply.github.com>
2022-02-16 20:15:21 +0530

    Update Any number with Fee.cpp

--- Buy Sell Stock/Any number with Fee.cpp
+++ Buy Sell Stock/Any number with Fee.cpp
@@ -1,5 +1,48 @@
 class Solution {
 public:
+    int dp[50005][2];
+    int solve(vector<int>& prices, int fee, int idx, int buy){
+        if(idx >= prices.size()) return 0;
+        
+        // if i know the result of this state i will simply use that
+        if(dp[idx][buy] != -1) return dp[idx][buy];
+        
+        // i will have two options either to do nothing or to do something(buy/sell)
+        
+        int op1 = INT_MIN, op2 = INT_MIN;
+        
+        //option 1 -> to do nothing
+        
+        op1 = solve(prices, fee, idx + 1, buy);
+        
+        //option 2 -> to buy or sell
+        
+        // if i already bought some stock than i have to sell this
+        // because we cannot engage in multiple transactions
+        if(buy){
+            op2 = prices[idx] - fee + solve(prices, fee, idx + 1, 0);
+        }
+        
+        // else we haven't bought anything so we can buy a stock 
+        else{
+              op2 = -prices[idx] + solve(prices, fee, idx + 1, 1);
+        }
+        
+        return dp[idx][buy] = max(op1, op2);
+    
+    }
+    
+    
+    int maxProfit(vector<int>& prices, int fee) {
+        memset(dp, -1, sizeof(dp));
+        return solve(prices, fee, 0, 0);   
+    }
+};
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+class Solution {
+public:
     int maxProfit(vector<int>& p, int fee) {
      int n = p.size();
         if (n < 2) return 0;
