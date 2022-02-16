6c699f93ac897feca4b44b28936228785f7465ca
Tushar Amrit <48964465+tushar-amrit-6@users.noreply.github.com>
2022-02-16 20:17:02 +0530

    Update Atmost 2.cpp

--- Buy Sell Stock/Atmost 2.cpp
+++ Buy Sell Stock/Atmost 2.cpp
@@ -1,5 +1,44 @@
 class Solution {
 public:
+    int dp[100005][3][3];
+    int solver(vector<int>& prices, int idx, int bought, int transactions) {
+
+        if (idx >= prices.size() || transactions == 0) {
+            return 0;
+        }
+        
+        if (dp[idx][bought][transactions] != -1) {
+            return dp[idx][bought][transactions]; 
+        }
+
+        int profitWithBuying, profitWithSelling, profitWithSkipping;
+        profitWithSkipping = profitWithSelling = profitWithBuying = 0;
+
+        profitWithSkipping = solver(prices, idx+1, bought, transactions);
+
+        if (bought) {
+
+            // already have purchased the stock
+            profitWithSelling = prices[idx] + solver(prices, idx+1, 1-bought, transactions-1);  
+        }
+        else {
+
+            // no stocks bought so buy it
+            profitWithBuying = solver(prices, idx+1, 1-bought, transactions) - prices[idx];
+        }
+
+        return dp[idx][bought][transactions] = max(profitWithSkipping, max(profitWithBuying, profitWithSelling));
+    }
+    
+    int maxProfit(vector<int>& prices) {
+        memset(dp, -1, sizeof(dp));
+        return solver(prices, 0, 0, 2);
+    }
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class Solution {
+public:
     int maxProfit(vector<int>& prices) {
         if(!prices.size())
             return 0;
