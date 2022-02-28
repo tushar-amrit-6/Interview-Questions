6668f2ee835d16412f7ece5f0f970ba4192f0697
Tushar Amrit <48964465+tushar-amrit-6@users.noreply.github.com>
2022-02-28 20:46:14 +0530

    Update Word Break.cpp

--- Word Break.cpp
+++ Word Break.cpp
@@ -1,3 +1,4 @@
+// CHECK IF POSSIBLE
 #include <unordered_set>
 
 // Declare a hash map.
@@ -46,3 +47,35 @@ bool wordBreak(vector < string > & arr, int n, string & target) {
     // Call wordBreakHelper to return answer.
     return wordBreakHelper(target, 0, dp);
 }
+
+
+//RETURN ALL COMBINATION
+class Solution {
+    unordered_map<string, vector<string>> m;
+
+    vector<string> combine(string word, vector<string> prev){
+        for(int i=0;i<prev.size();++i){
+            prev[i]+=" "+word;
+        }
+        return prev;
+    }
+
+public:
+    vector<string> wordBreak(string s, unordered_set<string>& dict) {
+        if(m.count(s)) return m[s]; //take from memory
+        vector<string> result;
+        if(dict.count(s)){ //a whole string is a word
+            result.push_back(s);
+        }
+        for(int i=1;i<s.size();++i){
+            string word=s.substr(i);
+            if(dict.count(word)){
+                string rem=s.substr(0,i);
+                vector<string> prev=combine(word,wordBreak(rem,dict));
+                result.insert(result.end(),prev.begin(), prev.end());
+            }
+        }
+        m[s]=result; //memorize
+        return result;
+    }
+};
